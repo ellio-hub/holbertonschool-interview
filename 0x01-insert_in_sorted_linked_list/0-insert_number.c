aca26b8ed96414e90e47ca39d90add859cea07c1
vito <mansourali1919@gmail.com>
2021-07-29 19:41:47 +0100

    1st push

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -5,7 +5,7 @@
  * insert_node - function that inserts number into sorted singly linked list
  * @head: first node in list
  * @number: number to be inserted
- * 
+ *
  * Return: the address of the new node, or NULL if it failed
 */
 listint_t *insert_node(listint_t **head, int number)
